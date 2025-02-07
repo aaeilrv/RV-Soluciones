8ee3fde51eeee5a8bd92ee1ae919cdbbc40550b1
Valeria <16-11233@usb.ve>
2025-02-07 00:16:02 -0400

    creating services page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,17 @@ import ContactCard from "@/components/ContactCard";
 import References from "@/components/References";
 import Benefits from "@/components/Benefits";
 import Products from "@/components/Products";
+import Services from "@/components/Services";
 
 export default function Home() {
   return (
     <div>
       <Hero/> {/** Falta logo en mobile */}
       <ContactCard/> {/** falta formulario + imágenes y ver qué se hace en mobile */}
-      <References/> {/** Faltan logos más grandes en mobile */}
+      <References/>
       <Benefits/>
       <Products/>
+      <Services/>
     </div>
   );
 }
